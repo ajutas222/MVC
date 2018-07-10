b74173401ff793e9e248a209726a2024acbf9772
Jutas <jarthasarnprasit@hexagroup.com>
2018-07-10 07:29:34 -0500

    Learning about Web API

---
+++ vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using vidly.Dtos;
+using vidly.Models;
+
+namespace vidly.App_Start
+{
+    public class MappingProfile : Profile  
+    {
+        // Constructor
+        public MappingProfile()
+        {
+            // Source to target
+            // Convention Mapping
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+        }
+    }
+}
---
+++ vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+
+namespace vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie's name.")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDateTime { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        [Required]
+        public byte NumberInStock { get; set; }
+    }
+}
